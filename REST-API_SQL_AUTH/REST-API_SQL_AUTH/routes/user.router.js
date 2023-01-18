25dc32382e30780ad5e86c9ebad8d7aa09b2cfd4
Jampyn <100176998+Jampyn@users.noreply.github.com>
2023-01-18 17:17:23 +0700

    Add files via upload

--- REST-API_SQL_AUTH/REST-API_SQL_AUTH/routes/user.router.js
+++ REST-API_SQL_AUTH/REST-API_SQL_AUTH/routes/user.router.js
@@ -5,24 +5,24 @@ module.exports = function (app) {
   app.use(function (req, res, next) {
     res.header(
       "Access-Control-Allow-Headers",
-      "x-access-token, Origin, Content-Type, Accept"
+      "X-access-token, Origin, Content-Type, Accept"
     );
     next();
   });
-  //http://localhost:5000/apis/test/all
+  // http://localhost:5000/apis/auth/all
   app.get("/apis/test/all", controller.allAccess);
 
-  //http://localhost:5000/apis/test/user
+  // http://localhost:5000/apis/auth/user
   app.get("/apis/test/user", [authjwt.verifyToken], controller.userBoard);
 
-  //http://localhost:5000/apis/test/admin
+  // http://localhost:5000/apis/auth/admin
   app.get(
     "/apis/test/admin",
     [authjwt.verifyToken, authjwt.isAdmin],
     controller.adminBoard
   );
 
-  //http://localhost:5000/apis/test/mod
+  // http://localhost:5000/apis/auth/mod
   app.get(
     "/apis/test/mod",
     [authjwt.verifyToken, authjwt.isModerator],
