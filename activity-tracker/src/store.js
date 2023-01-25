c97e836d3a6e5133ba7de10c62198b3408d4e368
Jampyn <100176998+Jampyn@users.noreply.github.com>
2023-01-25 15:42:07 +0700

    Add files via upload

---
+++ activity-tracker/src/store.js
@@ -0,0 +1,11 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { composeWithDevTools } from "redux-devtools-extension";
+import activitiesReducer from "./reducers/activitiesReducer";
+
+const store = configureStore({
+    reducer:{
+        activities: activitiesReducer
+    }
+}, composeWithDevTools);
+
+export default store;
